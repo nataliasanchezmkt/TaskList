83ce7b08c57aa7841cf33885955156cd9416f941
natalia sanchez <nataliasanchezmkt@gmail.com>
2022-01-08 20:31:54 -0300

    Rama master en limpio

--- src/components/FormTask.js
+++ src/components/FormTask.js
@@ -5,21 +5,14 @@ import List from './List'
 const FormTask = () => {
 
     let tareasLocal=JSON.parse( localStorage.getItem('taskList'))|| [];
-    // state para guardar el arreglo de tareas
-    // todo lo q se cambia en el maquetado y es un valor q en el tiempo va a cambiar, lo agrego al state ya que renderiza estos cambios con pocas lineas de codigo
-//inicializar el state con lo q sea q tenga la var tareasLocal, asi le decimos q en montaje busque las tareas q hay en el local
+    
 
     const [arregloTareas, setArregloTareas]=useState(tareasLocal);
-
-    // componenete controlado(asiganarle a cada elemento de un form ,como un input, un state)
     const[tarea, setTarea]= useState(''); 
 
 
-    // ciclo de vida(useEffect)
     useEffect(()=>{
-        // toda la logica q quiero usar en el ciclo de vida
-        //asi como esta escrito el useEffect(()=>{}) : funcionaria tanto en montaje como en  actualizacion de este componenete
-        //si quiero q sea solo en montaje: agrego un parametro al final de la cfuncion anonima  useEffect(()=>{},[]) puedo poner por eljemplo [arrgloTareas] para q preste atencion en ese state, presta atencion al montaje ya ctualiacion de arrglo tareas
+        
         localStorage.setItem('taskList',JSON.stringify(arregloTareas));
 
 
@@ -29,12 +22,11 @@ const FormTask = () => {
 
 
  
-// pra funciones q manejan un evento react suguiere usar handle+elnombre del evvetno q esta sucediendo
+
     const handleSubmit = (e)=>{
         e.preventDefault();
         
-        //guardar una tarea en el state
-        //operador spread ... se usa en todo lo q es iterable 
+        
 
         setArregloTareas([...arregloTareas,tarea]);
        //limpiar el input
@@ -50,13 +42,7 @@ const FormTask = () => {
         setArregloTareas(newArray)
 
     }
-    // funciones de una linea: pueden ir dentro del evento, son anonimas
-    // sintaxis: {()=>{}} peor puedo emitir las llaves amrillas q pertenes a la opcion anonima, cuando tengo solo 1 parametro puedo omitir los parentesis pero si no tengo o tengo ams de uno no puedo omitirlo
-    // no hace falta traer con document.getelement porque esta tdo junto para eeso se utiliza el objeto event tiene info del evento q esta sucediendo en ese instante de tiempo
-    // event existe en js tien info de todo lo q sucede
-    // por eso esta escrito en ese inout asi regirtra q estamos apuntando a ese input
-    // objeto event.target acceso directo al input
-    //funcion del state pasa como parametro lo q quiero cambiar 
+    
     
     return (
        <section className='form-style '>
@@ -73,8 +59,7 @@ const FormTask = () => {
            </Button>
            </Form.Group>
        </Form>
-       {/* mando la lista de tareas por props, el nomvre de la izquierda es l nombre del prop y lo de la derecha lo q quieor mandar */}
-       {/* cuando mando la funcion es sin parenteissi y si mando con parentesis se va a ejecutar en ese momento */}
+      
        <List stateTareas={arregloTareas} deleteTask ={deleteTask}/>
        </section>
     );
--- src/components/List.js
+++ src/components/List.js
@@ -3,15 +3,12 @@ import { ListGroup } from 'react-bootstrap';
 import Item from './Item'
 
 
-// pros es un objeto y para poder usarlo lo indicamos en el parentsisi del componenete que recibe el props
 
 
 function List(props) {
     return (
        <ListGroup >
-         {/* valor= parametro al quiero acceder del arreglo */}
-         {/* segundi parametro: es la posicion se puede usar como no pero ,e ayuda a indenticar al elemento del arreglo si no tengo potra menra de diferenciarlo */}
-         {/* key es un prop creado por react que debemos usar para establecer eso q lo hace unico */}
+       
          {props.stateTareas.map((valor,posicion)=><Item key={posicion} task={valor} deleteTask = {props.deleteTask}/>)}
    
        </ListGroup>
@@ -20,5 +17,3 @@ function List(props) {
 
 export default List;
 
-// simepre resolver los warning rojos
-// cad hijo en una lisra tiene q ser unico no pueden ser iguales
